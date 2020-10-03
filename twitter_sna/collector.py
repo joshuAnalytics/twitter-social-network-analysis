7640bbf0051eb8cca0fceb4a1e929cd49f6d7ed8
josh <joshua.bramall@excession.co>
2020-10-03 19:29:52 +0100

    added pathlib, dump jlb on user add, print statements

--- twitter_sna/collector.py
+++ twitter_sna/collector.py
@@ -2,11 +2,12 @@ import tweepy as tw
 import secrets as s
 import joblib as jlb
 import pandas as pd
+from pathlib import Path
 import os
 
 
 class DataCollector:
-    def __init__(self, tweepy_api, cache_path="_cache", cache_file="df_users.jlb"):
+    def __init__(self, tweepy_api, cache_path="_cache/", cache_file="df_users.jlb"):
         self.api = tweepy_api
         self.user_fields = [
             "id",
@@ -21,7 +22,7 @@ class DataCollector:
         self.follower_fields = ["id_str", "name", "screen_name", "followers_count", "friends_count", "created_at"]
         self.cache_path = cache_path
         self.cache_file = cache_file
-        self.cache_file_path = os.path.join(cache_path, cache_file)
+        self.cache_file_path = Path(cache_path, cache_file)
         if os.path.exists(self.cache_file_path):
             self.df_users = jlb.load(self.cache_file_path)
         else:
@@ -46,6 +47,7 @@ class DataCollector:
             response = self.api.get_user(handle)
             user_row = self.parse_user_data(response, self.user_fields)
             self.df_users = self.df_users.append(user_row, sort=False)
+            jlb.dump(self.df_users, self.cache_file_path)
             print(f"{handle} added")
         else:
             print(f"{handle} already cached")
@@ -98,8 +100,9 @@ class DataCollector:
         return self.df_users[self.df_users["screen_name"] == handle]["followers_count"]
 
     def _parse_followers_data(self, handle, response, followers_fields):
-        # parses the followers tweepy response to pandas df
-        # convert the followers object to a dict
+        """
+        parses the followers tweepy response to pandas df
+        """
         user_dict = vars(response)["_json"]
         follower_dict = {}
         for field in followers_fields:
@@ -110,6 +113,9 @@ class DataCollector:
         return follower_dict
 
     def rate_limit_remaining(self):
+        """
+        get the remaining requests info from twitter
+        """
         return self.api.rate_limit_status()["resources"]["application"]["/application/rate_limit_status"]["remaining"]
 
     def get_followers(self, handle):
@@ -118,6 +124,7 @@ class DataCollector:
         """
         handle = self._fix_handle(handle)
         followers_df = pd.DataFrame()
+
         # check the user is cached, if not retrive user stats
         if not self.is_handle_cached(handle):
             self.add_user(handle)
@@ -126,13 +133,13 @@ class DataCollector:
         followers_df = pd.DataFrame(columns=self.follower_fields)
         followers_df.to_csv(followers_df_path, index=False)
 
+        print("remaining requests before rate limit:")
         # request the followers using tweepy
-        for response in tw.Cursor(self.api.followers, handle).items(5):
-            # handle = self._fix_handle(response.screen_name)
+        for response in tw.Cursor(self.api.followers, handle).items(2000):
             followers_df = pd.read_csv(followers_df_path)
             followers_df = followers_df.append(
                 self._parse_followers_data(handle, response, self.follower_fields), ignore_index=True
             )
             followers_df["follows"] = handle
             followers_df.to_csv(followers_df_path, index=False)
-            print(f"--- rate limit status: {self.rate_limit_remaining()} remaining ---")
+            print(self.rate_limit_remaining(), end="\r")
